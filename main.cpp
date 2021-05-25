61095a3c0583f853282b625113d76532812e8dbd
liruisong82 <57514218+liruisong82@users.noreply.github.com>
2021-05-25 22:24:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,56 @@
-#ifndef PERSONPAGE_H
-#define PERSONPAGE_H
+#include "mainwindow.h"
+#include "STUlogin.h"
+#include "stulogin.h"
+#include <QApplication>
+#include<QDebug>
+#include<QSqlDatabase>
+#include <QSqlQuery>
 
-#include <QMainWindow>
-#include <QDialog>
+bool opendatabase();
 
-namespace Ui {
-class personpage;
+int main(int argc, char *argv[])
+{
+    opendatabase();
+    QApplication a(argc, argv);
+    STUlogin w;
+    w.show();
+
+    return a.exec();
+}
+bool opendatabase()
+{
+    QSqlDatabase mydb=QSqlDatabase::addDatabase("QSQLITE");
+    mydb.setDatabaseName("content.db");//release用
+    if(mydb.open())
+    {
+        //qDebug()<<"open success";
+        QSqlQuery query;
+       // QString create_user = "create table user_ (username varchar(50) primary key, password varchar(50))";
+        QString create_cont = "create table content_ (title varchar(500) primary key, username varchar(50) ,content varchar(4000),foreign key(username)references user(username))";
+
+        QString create_user = "create table user (id int primary key, username varchar(50), password varchar(50))";
+//        QString create_con = "create table content (ID int primary key, username varchar(50) , title varchar(500),content varchar(4000),foreign key(username)references user(username))";
+
+        query.prepare(create_user);
+        query.prepare(create_cont);
+
+        if(!query.exec())
+        {
+            qDebug() << "Error: Fail to create table.";
+        }
+        else
+        {
+            qDebug() << "Table created!";
+        }
+        return true;
+    }
+    else
+    {
+        qDebug()<<"open failed";
+        return false;
+    }
+
+
 }
 
-class personpage : public QMainWindow
-{
-    Q_OBJECT
 
-public:
-    explicit personpage(QWidget *parent = 0);
-    ~personpage();
-    void init();
-    QString pgname;
-
-
-private slots:
-    void on_pushButton_mycon_clicked();
-
-private:
-    Ui::personpage *ui;
-};
-
-#endif // PERSONPAGE_H
